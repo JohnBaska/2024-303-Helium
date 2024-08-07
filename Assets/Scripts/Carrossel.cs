ee2bb7e7fde84e009d543ee5e525f8081fa56cda
Pedro Henrique <pedro.26.3.h@gmail.com>
2024-08-07 16:04:01 -0300

    Problema do carrosel

--- Assets/Scripts/Carrossel.cs
+++ Assets/Scripts/Carrossel.cs
@@ -1,26 +1,25 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Carrossel : MonoBehaviour
-{
+public class Carrossel : MonoBehaviour { 
     [SerializeField]
     private float velocidade;
+
     private Vector3 posicaoInicial;
-    private float tamanhoRealDaImagem;
-    private void Awake()
-    {
+    private float tamanhoRealDaImagem ;
+
+    private void Awake() {
         this.posicaoInicial = this.transform.position;
         float tamanhoDaImagem = this.GetComponent<SpriteRenderer>().size.x;
         float escala = this.transform.localScale.x;
         this.tamanhoRealDaImagem = tamanhoDaImagem * escala;
     }
-    private void Update()
-    {
-        // x = v0t * imagem.x;
+
+    void Update() {
         float deslocamento = Mathf.Repeat(this.velocidade * Time.time, this.tamanhoRealDaImagem);
-        // Objeto.x = x0 + x;
+        Debug.Log(gameObject.name + ": " + deslocamento);
+        
         this.transform.position = this.posicaoInicial + Vector3.left * deslocamento;
     }
 }
